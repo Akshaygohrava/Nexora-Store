029e9aeab736e7fba238601b763980853200a6e4
Akshay Gohrava <akshaygohrava@gmail.com>
2025-01-22 21:35:22 +0530

    An Nexora-Store

---
+++ src/app/page.js
@@ -0,0 +1,203 @@
+'use client';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MouseIcon from '@mui/icons-material/Mouse';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import Grid from '@mui/material/Grid';
+import RecipeReviewCard from './RecipeReviewCard';
+import Link from '@mui/material/Link';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+const Footer = () => (
+  <Box
+    component="footer"
+    sx={{
+      backgroundColor: '#f8f9fa',
+      color: '#6c757d',
+      textAlign: 'center',
+      py: 3,
+      mt: 4,
+      borderTop: '1px solid #e9ecef',
+    }}
+  >
+    <Typography variant="body1">
+      &copy; 2025 By Akshay Gohrava
+    </Typography>
+    <Typography variant="body2">
+      <Link href="mailto:akshaygohrava@gmail.com" underline="hover">
+        akshaygohrava@gmail.com
+      </Link>
+      {' | '}
+      <Link href="tel:+918452070571" underline="hover">
+        Call Us
+      </Link>
+    </Typography>
+  </Box>
+);
+
+function ResponsiveAppBar() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <MouseIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Nexora
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: 'flex',
+                justifyContent: 'flex-end',
+                ml: 'auto',
+              }}
+            >
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar sx={{ bgcolor: '#81c784', color: 'white' }}>A</Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      {/* Card UI Rendered Below the AppBar */}
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={4} justifyContent="center">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={8}
+              md={4}
+              key={index}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <RecipeReviewCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+
+      {/* Footer Component */}
+      <Footer />
+    </>
+  );
+}
+
+export default ResponsiveAppBar;
