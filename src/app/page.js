a7542b1a183d8eb1becf3551139ad13e85574c7d
Akshay Gohrava <akshaygohrava@gmail.com>
2025-11-21 11:51:25 +0530

    An Basic Update

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,15 @@ export default function Home() {
   return (
     <>
     <div className="m-10">
-       <h1 className="text-center text-3xl">Hello, Welcome To My Nexora-Store</h1>
-
+       <h1 style={{
+        fontFamily: "cursive",
+        color: "red"
+       }} className="text-center text-3xl">Hello, Welcome To My Nexora-Store</h1>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
 
 
     </div>
